940cb62a8e3e9196598c886f33495751aabfae1f
Lukas Polacek <lukiy223344@gmail.com>
2023-10-10 20:37:11 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use console::{Style, Term};
+use dialoguer::{theme::ColorfulTheme, Input};
 use std::collections::VecDeque;
+use std::fmt::format;
+use std::thread;
+use std::time::Duration;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone)] //more specific types like [str(name)] todo str to not accept "12"
 enum DataType {
     Id,
     Int,
     Str,
     Bool,
-    Some(String),//todo autoincrement id
+    Some(String), //todo autoincrement id
 }
 #[derive(Debug, Clone)]
 struct Header {
@@ -67,7 +72,6 @@ struct Table {
 
 impl Table {
     fn new(path: &str) -> Table {
-
         let f = std::fs::read_to_string(path).unwrap();
 
         let lines_u = f.lines().collect::<Vec<&str>>();
@@ -99,17 +103,60 @@ impl Table {
         for (i, header) in self.headers.iter().enumerate() {
             self.columns.push(Column::new((*header).clone()));
             for line in lines {
-                self.columns[i].content.push(line[i].to_string());
+                self.columns[i].content.push(line[i].to_string().clone());
             }
         }
     }
-}
 
-fn main() {
-    let table = Table::new("src/test.rat");
-    println!("{:?}", table.headers);
-    for j in table.columns {
-        println!("{:?}", j.content);
+    fn add_row(&mut self, row: Vec<String>) {
+        for (i, val) in row.iter().enumerate() {
+            self.columns[i].content.push(val.to_string().clone());
+        }
     }
+
+    fn input_row(&mut self) {
+        let mut row = Vec::new();
+        for header in self.headers.iter() {
+            let input: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("{} with type [{:?}]", header.name, header.datatype))
+                .validate_with(|input:&String| {
+                    match header.datatype{
+                        DataType::Int | DataType::Id => {match input.parse::<i64>() {
+                            Ok(val) => Ok(()),
+                            Err(e) => Err("should be integer")
+                        }},
+                        DataType::Str => {match input.parse::<String>() {
+                            Ok(val) => Ok(()),
+                            Err(e) => Err("should be String")
+                        }},
+                        DataType::Bool => {match input.parse::<bool>() {
+                            Ok(val) => Ok(()),
+                            Err(e) => Err("should be bool")
+                        }},
+                        _ => panic!() // todo more checks for bools to accept "true"
+                    }
+                })
+                .interact_text()
+                .unwrap();
+
+            row.push(input)
+        }
+        self.add_row(row);
+    }
+
+    fn select(&mut self, header: &str) -> Result<&Vec<String>, &'static str>{
+        for column in self.columns.iter(){
+            if column.header.name.as_str() == header {
+                return Ok(&column.content);
+            }
+        }
+        Err("empty table or nothing found")
+    }
+}
+fn main() {
+    let mut table = Table::new("src/test.rat");
+    // table.input_row();
+    println!("{:?}", table.select("name").unwrap())
+    // todo use dialoguer
 
 }
